1c102f5abcda5b98fe156c3c6fffcefaa522a5de
Ashwin Khadka <ashwinkhadka027@gmail.com>
2026-07-11 21:13:37 +0545

    ui

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,10 @@
+// src/configs/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  // During development (npm run dev), we want the baseURL to be '/'
+  // In production, we'll use an environment variable.
+  baseURL: import.meta.env.MODE === 'development' ? '/' : import.meta.env.VITE_BASE_URL,
 });
 
 export default api;
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // !!! ENSURE THIS LINE IS EXACTLY CORRECT !!!
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
