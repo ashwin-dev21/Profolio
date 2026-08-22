b1c400e7e754152c27b0a47e0f79b93a1c8777d1
Ashwin Khadka <ashwinkhadka027@gmail.com>
2026-08-22 21:47:59 +0545

    routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,8 @@ import {
   getUserResumes,
   loginUser,
   registerUser,
+  forgotPassword,
+  resetPassword,
 } from "../controllers/userController.js";
 import protect from "../middlewares/authMiddleware.js";
 
@@ -11,6 +13,8 @@ const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.post("/forgot-password", forgotPassword); // <-- Added
+userRouter.post("/reset-password/:token", resetPassword); // <-- Added
 userRouter.get("/data", protect, getUserById);
 userRouter.get("/resumes", protect, getUserResumes);
 
